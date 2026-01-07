c9491d8b9600e0c8903d428c59429a92a5a74c73
chaitanya <chaitanya@gmail.com>
2026-01-07 10:45:11 +0530

    added subnet 2

--- vpc.tf
+++ vpc.tf
@@ -15,3 +15,13 @@ resource "aws_subnet" "demosubnet-1" {
     "Name" = "DemoGIT-Subnet-1"
   }
 }
+
+resource "aws_subnet" "demosubnet-2" {
+  vpc_id = aws_vpc.demovpc.id
+  cidr_block = "10.12.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-south-1b"
+  tags = {
+    "Name" = "DemoGIT-Subnet-2"
+  }
+}
